8034162ec0aaeee138220c7ae2cb6fd8833f9b6f
Vinay Gawade <vinulike11@gmail.com>
2023-11-19 01:23:21 +0530

    feat: new svg icons & minor changes

--- renderer.js
+++ renderer.js
@@ -29,12 +29,12 @@ function createItemTemplate(itemID, text) {
     <button class="clipboard" itemID="` +
     itemID +
     `">
-      <span class="icon icon-clipboard"></span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path fill="#1c6ec1" d="M216 32H88a8 8 0 0 0-8 8v40H40a8 8 0 0 0-8 8v128a8 8 0 0 0 8 8h128a8 8 0 0 0 8-8v-40h40a8 8 0 0 0 8-8V40a8 8 0 0 0-8-8Zm-8 128h-32V88a8 8 0 0 0-8-8H96V48h112Z"/></svg>
     </button>
     <button class="delete" itemID="` +
     itemID +
     `">
-      <span class="icon icon-delete"></span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 36 36"><path fill="red" d="M27.14 34H8.86A2.93 2.93 0 0 1 6 31V11.23h2V31a.93.93 0 0 0 .86 1h18.28a.93.93 0 0 0 .86-1V11.23h2V31a2.93 2.93 0 0 1-2.86 3Z" class="clr-i-outline clr-i-outline-path-1"/><path fill="red" d="M30.78 9H5a1 1 0 0 1 0-2h25.78a1 1 0 0 1 0 2Z" class="clr-i-outline clr-i-outline-path-2"/><path fill="red" d="M21 13h2v15h-2z" class="clr-i-outline clr-i-outline-path-3"/><path fill="red" d="M13 13h2v15h-2z" class="clr-i-outline clr-i-outline-path-4"/><path fill="red" d="M23 5.86h-1.9V4h-6.2v1.86H13V4a2 2 0 0 1 1.9-2h6.2A2 2 0 0 1 23 4Z" class="clr-i-outline clr-i-outline-path-5"/><path fill="none" d="M0 0h36v36H0z"/></svg>
     </button>
   </div>`
   )
@@ -71,9 +71,9 @@ $(function() {
   writeItems(items)
 })
 
-$(".minimize").on("click", function() {
-  ipcRenderer.send("hide-window")
-})
+// $(".minimize").on("click", function() {
+//   ipcRenderer.send("hide-window")
+// })
 
 $(".exit").on("click", function() {
   Swal.fire({
